4d789f7846244376a902f36b24db784c3c902d94
enthus-io <lfojedaguzman@gmail.com>
2021-02-25 18:34:18 -0600

    Added solution for day 04

--- Java/day00/src/Solution.java
+++ Java/day00/src/Solution.java
@@ -1,29 +1,3 @@
-package Solution;
-/*
-Objective
-In this challenge, we review some basic concepts
-that will get you started with this series. You will
-need to use the same (or similar) syntax to read
-input and write output in challenges throughout
-challenges.
-
-To complete this challenge, you must save a line of
-input from stdin to a variable, print Hello, World.
-on a single line, and finally print the value of
-your variable on a second line.
-
-Output Format
-Print Hello, World. on the first line, and the
-contents of on the second line.
-
-Sample Input
-Welcome to 30 Days of Code!
-
-Sample Output
-Hello, World.
-Welcome to 30 Days of Code!
-*/
-
 import java.io.*;
 import java.util.*;
 import java.text.*;
