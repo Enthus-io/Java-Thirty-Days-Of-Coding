cbc55b0aff8d04238c8edf250364453ec4613817
Enthus-io <lfojedaguzman@gmail.com>
2021-03-02 14:01:09 -0600

    Added the solution to day 10

--- Java/day10 Binary Numbers/src/Solution.java
+++ Java/day10 Binary Numbers/src/Solution.java
@@ -16,6 +16,27 @@ public class Solution {
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+        int base = 2;
+        int currentConsecutives = 0;
+        int maxConsecutives = 0;
+
+        while(n > 0){
+            int remainder = n % base;
+
+            if(remainder == 1) {
+                currentConsecutives += remainder;
+
+                if(currentConsecutives > maxConsecutives)
+                    maxConsecutives = currentConsecutives;
+            }
+            else
+                currentConsecutives = 0;
+
+            n /= base;
+        }
+
+        System.out.println(maxConsecutives);
+
         scanner.close();
     }
 }
