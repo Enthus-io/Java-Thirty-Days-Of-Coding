15b7eb35fd7a31e787329346d2be9b3a81ee66e7
Enthus-io <lfojedaguzman@gmail.com>
2021-03-21 12:44:42 -0500

    Added my solution to day 16

--- Java/Day 16 Exceptions - String to Integer/src/Solution.java
+++ Java/Day 16 Exceptions - String to Integer/src/Solution.java
@@ -9,5 +9,15 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
+
+        try {
+            System.out.println(Integer.parseInt(S));
+        }
+        catch (Exception e){
+            System.out.println("Bad String");
+        }
+        finally {
+            System.out.println("This print always.");
+        }
     }
 }
