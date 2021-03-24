371260e758b46274e3b15ed0d6ae0a566e31c232
Enthus-io <lfojedaguzman@gmail.com>
2021-03-24 11:55:35 -0500

    Added my solution to day 22

--- Java/Day 22 Binary Search Trees/src/Solution.java
+++ Java/Day 22 Binary Search Trees/src/Solution.java
@@ -15,6 +15,16 @@ class Solution {
 
     public static int getHeight(Node root) {
         //Write your code here
+        if (root == null)
+            return -1;
+
+        int l = getHeight(root.left);
+        int r = getHeight(root.right);
+
+        if (l >= r)
+            return l + 1;
+        else
+            return r + 1;
     }
 
     public static Node insert(Node root, int data) {
