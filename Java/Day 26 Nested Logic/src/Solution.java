53ecc7c0164b23bdd26a3121db3b56c5a73d7ab2
enthus-io <lfojedaguzman@gmail.com>
2021-04-02 08:28:35 -0500

    My solution to day 26

--- Java/Day 26 Nested Logic/src/Solution.java
+++ Java/Day 26 Nested Logic/src/Solution.java
@@ -5,6 +5,26 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner mScanner = new Scanner(System.in);
+        int returnDay = mScanner.nextInt();
+        int returnMonth = mScanner.nextInt();
+        int returnYear = mScanner.nextInt();
 
+        int dueDay = mScanner.nextInt();
+        int dueMonth = mScanner.nextInt();
+        int dueYear = mScanner.nextInt();
+        mScanner.close();
+
+        int fine = 0;
+
+        if (returnDay > dueDay && returnMonth == dueMonth && returnYear == dueYear) {
+            fine = 15 * (returnDay - dueDay);
+        } else if (returnMonth > dueMonth && returnYear == dueYear) {
+            fine = 500 * (returnMonth - dueMonth);
+        } else if (returnYear > dueYear) {
+            fine = 10000;
+        }
+
+        System.out.println(fine);
     }
 }
