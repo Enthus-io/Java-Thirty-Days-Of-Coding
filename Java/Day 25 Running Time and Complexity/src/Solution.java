12016a669d4d4bc386e4ed49dee4e4c5d74f214c
enthus-io <lfojedaguzman@gmail.com>
2021-04-02 02:57:41 -0500

    Added my solution to day 25

--- Java/Day 25 Running Time and Complexity/src/Solution.java
+++ Java/Day 25 Running Time and Complexity/src/Solution.java
@@ -1,9 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int e = sc.nextInt();
+
+            if (!isPrime(e)) System.out.println("Not prime");
+            else System.out.println("Prime");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+
+        if ((n > 2 && n % 2 == 0) || n == 1) {
+            return false;
+        }
+
+        for (int i = 3; i <= (int) Math.sqrt(n); i += 2) {
+
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
