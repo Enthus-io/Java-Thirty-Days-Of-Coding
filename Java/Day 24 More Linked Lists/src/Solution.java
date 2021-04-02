9ab804e9863888d1cbc379f28de9e4de4af18933
enthus-io <lfojedaguzman@gmail.com>
2021-04-02 02:00:14 -0500

    Added my solution to day 24

--- Java/Day 24 More Linked Lists/src/Solution.java
+++ Java/Day 24 More Linked Lists/src/Solution.java
@@ -16,6 +16,20 @@ class Solution {
 
     public static Node removeDuplicates(Node head) {
         // Write your code here
+        if (head == null) return null;
+
+        Node temp = head;
+
+        while (temp.next != null) {
+
+            if (temp.data == temp.next.data) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+
+        return head;
     }
 
 
