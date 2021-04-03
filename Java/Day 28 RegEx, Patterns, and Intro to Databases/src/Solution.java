253d94f77cdd92ccd526b846df89180225dce1ea
enthus-io <lfojedaguzman@gmail.com>
2021-04-03 02:31:53 -0500

    Added my solution to day 28

--- Java/Day 28 RegEx, Patterns, and Intro to Databases/src/Solution.java
+++ Java/Day 28 RegEx, Patterns, and Intro to Databases/src/Solution.java
@@ -8,20 +8,31 @@ import java.util.regex.*;
 
 public class Solution {
 
-
-
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        Pattern p = Pattern.compile("[a-z]+@gmail\\.com$");
+        ArrayList<String> list = new ArrayList<>();
 
-        for (int NItr = 0; NItr < N; NItr++) {
+        for (int i = 0; i < N; i++) {
             String[] firstNameEmailID = scanner.nextLine().split(" ");
 
             String firstName = firstNameEmailID[0];
-
             String emailID = firstNameEmailID[1];
+
+            Matcher m = p.matcher(emailID);
+            if (m.find()) {
+                list.add(firstName);
+            }
+        }
+
+        Collections.sort(list);
+
+        for (String name : list) {
+            System.out.println(name);
         }
 
         scanner.close();
